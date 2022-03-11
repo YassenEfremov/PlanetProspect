dd265b5a094ff1f91771ba051d6c78209fbd16cf
Squidfishrl <ivo.genchev4@gmail.com>
2022-03-11 17:50:55 +0200

    Solve Kepler's equation

--- Assets/Scripts/SolarSystemSimulation/PlanetaryOrbit.cs
+++ Assets/Scripts/SolarSystemSimulation/PlanetaryOrbit.cs
@@ -32,9 +32,45 @@ public class PlanetaryOrbit : MonoBehaviour {
     public Vector3 CalculateCoordinates(double julianCenturiesSinceEpoch) {
         Vector3 coords = new Vector3();
         
-        // TODO
+        // Compute values for the Kepler elements for the given (Julian) time
+        float semiMajorAxis = (float)(this.semiMajorAxis.element + this.semiMajorAxis.rate * julianCenturiesSinceEpoch);
+        float eccentricity = (float)(this.eccentricity.element + this.eccentricity.rate * julianCenturiesSinceEpoch);
+        float inclination = (float)(this.inclination.element + this.inclination.rate * julianCenturiesSinceEpoch);
+        float meanLongitude = (float)(this.meanLongitude.element + this.meanLongitude.rate * julianCenturiesSinceEpoch);
+        float perihelionLongitude = (float)(this.perihelionLongitude.element + this.perihelionLongitude.rate * julianCenturiesSinceEpoch);
+        float ascendingNodeLongitude = (float)(this.ascendingNodeLongitude.element + this.ascendingNodeLongitude.rate * julianCenturiesSinceEpoch);
+
+        // Compute the argument of perihelion and the mean anomaly
+        float argumentOfPerihelion = perihelionLongitude - ascendingNodeLongitude;
+        float meanAnomaly = meanLongitude - perihelionLongitude;
+
+        // Modulus the mean anomaly so that -180 <= M <= +180
+        while (meanAnomaly > 180) {
+            meanAnomaly -= 360;
+        }
+
+        // Solve Kepler's equation (Newton's way) to get the eccentricity anomaly
+        float eccentricAnomaly = KeplerEquation(meanAnomaly, eccentricity, 5);
 
         return coords;
+    }
+
+    /*
+     * Calculate the eccentric anomaly given the mean anomaly and the eccentricity
+     * meanAnomaly = eccentricAnomaly - eccentricity * sin(eccentricAnomaly)
+     */
+    public static float KeplerEquation(float meanAnomaly, float eccentricity, uint precision) {
+        //E0
+        float eccentricAnomaly = meanAnomaly + eccentricity * Mathf.Sin(meanAnomaly);
+
+        for (uint i = 0; i < precision; i++) {
+            float deltaMeanAnomaly = meanAnomaly - (eccentricAnomaly - eccentricity * Mathf.Sin(eccentricAnomaly));
+            float deltaEccentricAnomaly = deltaMeanAnomaly / (1 - eccentricity * Mathf.Cos(eccentricAnomaly));
+            eccentricAnomaly += deltaEccentricAnomaly;
+        }
+
+        return eccentricAnomaly;
+
     }
 };
 
