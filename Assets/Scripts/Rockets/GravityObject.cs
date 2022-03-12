6abc3f31b32b6a2f6257de2ffbc70a799b98c02e
Squidfishrl <ivo.genchev4@gmail.com>
2022-03-13 00:27:35 +0200

    A lot of refactor

---
+++ Assets/Scripts/Rockets/GravityObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class GravityObject : MonoBehaviour {
+    public bool isGravityAffected;
+    public float radius;
+    public float surfaceGravity;
+    public Vector3 initialVelocity;
+
+    public Vector3 velocity {get; set;}
+    public float mass { get; private set; }
+    
+    // [System.NonSerialized]
+    private Rigidbody rb;
+
+    void Awake () {
+        rb = GetComponent<Rigidbody>();
+        rb.mass = mass;
+        velocity = initialVelocity;
+    }
+
+    void OnValidate () {
+        mass = surfaceGravity * radius * radius / Universe.gravitationalConstant;
+    }
+
+    public Rigidbody Rigidbody {
+        get {
+            return rb;
+        }
+    }
+
+    public Vector3 Position {
+        get {
+            return rb.position;
+        }
+
+        set {
+            rb.position = value;
+            rb.MovePosition(rb.position);
+            Quaternion rotation = Quaternion.LookRotation(velocity);
+            rb.MoveRotation(rotation);
+        }
+    }
+}
